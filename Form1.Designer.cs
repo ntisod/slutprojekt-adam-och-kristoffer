282c992bf1a62dd214b81093845d1d233a4b51e5
AdamTheBeast <43605392+AdamTheBeast@users.noreply.github.com>
2019-12-20 11:41:13 +0100

    Nästan färdig

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,128 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.pillar2 = new System.Windows.Forms.PictureBox();
+            this.pillar1 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gametick);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 364);
+            this.splitter1.TabIndex = 5;
+            this.splitter1.TabStop = false;
+            // 
+            // ufo
+            // 
+            this.ufo.BackColor = System.Drawing.Color.Transparent;
+            this.ufo.Image = global::flappyplane.Properties.Resources.alien1;
+            this.ufo.Location = new System.Drawing.Point(706, 102);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(68, 72);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ufo.TabIndex = 3;
+            this.ufo.TabStop = false;
+            this.ufo.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pillar2
+            // 
+            this.pillar2.Image = ((System.Drawing.Image)(resources.GetObject("pillar2.Image")));
+            this.pillar2.Location = new System.Drawing.Point(570, 230);
+            this.pillar2.Name = "pillar2";
+            this.pillar2.Size = new System.Drawing.Size(65, 180);
+            this.pillar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar2.TabIndex = 2;
+            this.pillar2.TabStop = false;
+            // 
+            // pillar1
+            // 
+            this.pillar1.BackColor = System.Drawing.Color.Transparent;
+            this.pillar1.Image = ((System.Drawing.Image)(resources.GetObject("pillar1.Image")));
+            this.pillar1.ImageLocation = "";
+            this.pillar1.Location = new System.Drawing.Point(338, -6);
+            this.pillar1.Name = "pillar1";
+            this.pillar1.Size = new System.Drawing.Size(65, 180);
+            this.pillar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar1.TabIndex = 1;
+            this.pillar1.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::flappyplane.Properties.Resources.Halicopter;
+            this.player.Location = new System.Drawing.Point(85, 188);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 54);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(795, 364);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ufo);
+            this.Controls.Add(this.pillar2);
+            this.Controls.Add(this.pillar1);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "FlappyHelicopter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pillar1;
+        private System.Windows.Forms.PictureBox pillar2;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Splitter splitter1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,17 +4,212 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace flappyplane
 {
     public partial class Form1 : Form
-    {
+    { 
+        
+        bool goup;
+        bool godown;
+        bool shot = false;
+        int score = 0;
+        int speed = 8; 
+        Random rand = new Random();
+        int playerSpeed = 7;
+        int index;
+        bool jumping = false;
+        int gravity = 2;
+
         public Form1()
         {
             InitializeComponent();
+           
+
         }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void changeUFO()
+        {
+            index += 1;
+            if (index > 3)
+            {
+                index = 1;
+            }
+
+            switch (index)
+            {
+                case 1:
+                    ufo.Image = Properties.Resources.alien1;
+                    break;
+
+                case 2:
+                    ufo.Image = Properties.Resources.alien2;
+                    break;
+
+                case 3:
+                    ufo.Image = Properties.Resources.alien3;
+                    break;
+
+            }
+
+        }
+
+        private void makeBullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.BackColor = System.Drawing.Color.DarkOrange;
+            bullet.Height = 5;
+            bullet.Width = 10;
+            bullet.Left = player.Left + player.Width;
+            bullet.Top = player.Top + player.Height / 2;
+            bullet.Tag = "bullet";
+            this.Controls.Add(bullet);
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = true;
+            }
+            if (e.KeyCode == Keys.R && shot == false)
+            {
+                makeBullet();
+                shot = true;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                jumping = true;
+                gravity = -5;
+
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = false;
+            }
+            if (shot == true)
+            {
+                shot = false;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                jumping = false;
+                gravity = 5;
+
+            }
+        }
+
+        private void gametick(object sender, EventArgs e)
+        {
+            pillar1.Left -= speed;
+            pillar2.Left -= speed;
+            ufo.Left -= speed;
+            label1.Text = "Score: " + score;
+            player.Top+= gravity;
+
+            if (goup)
+            {
+                player.Top -= playerSpeed;
+            }
+
+            if (godown)
+            {
+                player.Top += playerSpeed;
+            }
+
+            if (pillar1.Left < -150)
+            {
+                pillar1.Left = 900;
+            }
+
+            if (pillar2.Left < -150)
+            {
+                pillar2.Left = 1000;
+            }
+
+            if (ufo.Left < -5 ||
+                player.Bounds.IntersectsWith(ufo.Bounds) ||
+                player.Bounds.IntersectsWith(pillar1.Bounds) ||
+                player.Bounds.IntersectsWith(pillar2.Bounds)
+                 )
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Du misslyckades med uppdraget, du dödade " + score + " rymdskepp");
+                this.Close();
+
+            }
+
+            foreach (Control X in this.Controls)
+            {
+
+                if (X is PictureBox && X.Tag == "bullet")
+                {
+                    X.Left += 15;
+
+                    if (X.Left > 900)
+                    {
+                        this.Controls.Remove(X);
+
+                        X.Dispose();
+                    }
+                    if (X.Bounds.IntersectsWith(ufo.Bounds))
+                    {
+                        score += 1;
+                        this.Controls.Remove(X);
+                        X.Dispose();
+                        ufo.Left = 1000;
+                        ufo.Top = rand.Next(5, 330) - ufo.Height;
+                        changeUFO();
+                    }
+                }
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            SoundPlayer s = new SoundPlayer(@"C:\Users\adam.nasser1\Desktop\Music\DesiJourney.wav");
+
+                s.Play();         
+        }
+
+      
     }
 }
